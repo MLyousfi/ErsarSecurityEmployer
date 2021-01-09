04b8f5ceb51f6e0d76d8090e9413df1bbf924ae2
lyousfi529 <lyousfi529@gmail.com>
2021-01-09 21:32:09 +0100

    First version

---
+++ ErsarSecuriteEmployers/Program.cs
@@ -0,0 +1,75 @@
+﻿using ErsarSecuriteEmployers.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ErsarSecuriteEmployers
+{
+    static class Program 
+    {
+        public static DataAccess access;
+       public static string localAppFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+       public static string localAppFolderPath = Path.Combine(localAppFolder, "HalfShieldStudio");
+       public static string EmployerFolderPath = Path.Combine(localAppFolderPath, "EmployerData");
+       public static string sourceFilePath = Path.Combine(System.Windows.Forms.Application.StartupPath, "ERSARDATA.accdb");
+       public static string destFilePath = Path.Combine(localAppFolderPath, "ERSARDATA.accdb");
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            
+
+            var oledb12Installed = new System.Data.OleDb.OleDbEnumerator()
+                                        .GetElements().AsEnumerable()
+                                        .Any(x => x.Field<string>("SOURCES_NAME") ==
+                                            "Microsoft.ACE.OLEDB.12.0");
+            //MessageBox.Show(oledb12Installed.ToString());
+            CreateEmployerFolder();
+            Application.Run(new Form1());
+        }
+
+        public static void CreateEmployerFolder()
+        {
+            
+            if (!File.Exists(destFilePath))
+            {
+                File.Copy(sourceFilePath, destFilePath);
+
+            }
+
+            Properties.Settings.Default.DataCS = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}", destFilePath);
+            access = new DataAccess();
+            if (!Directory.Exists(EmployerFolderPath))
+                Directory.CreateDirectory(EmployerFolderPath);
+
+            
+            Properties.Settings.Default.EmployerFolderPath = EmployerFolderPath;
+
+            var res = access.ExecQuery(ConstantQuery.SelectAll, true);
+            if (res.success)
+            {
+                Properties.Settings.Default.rowsNum = res.RecordCount;
+                for (int i = 0; i < res.RecordCount; i++)
+                {
+                    Directory.CreateDirectory(Path.Combine(EmployerFolderPath, res.IDs.Pop()));
+                }
+            }
+
+
+        }
+
+    }
+   
+       
+}
---
+++ ErsarSecuriteEmployers/UserControl/ArchiveControl.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ErsarSecuriteEmployers.Data;
+using System.IO;
+using PagedList;
+
+namespace ErsarSecuriteEmployers
+{
+    
+    public partial class ArchiveControl : UserControl
+    {
+
+        public bool ColorIsCHnaged = false;
+        public ArchiveControl()
+        {
+            InitializeComponent();
+        }
+
+
+        
+
+        public void RefreshData()
+        {
+           
+            this.archive.DataSource = null;
+            this.archive.Rows.Clear();
+            var res = Program.access.ExecQuery(ConstantQuery.SelectAll);
+            if (res.success)
+            {
+                this.archive.DataSource = Program.access.DBDT;
+
+                Properties.Settings.Default.rowsNum = archive.Rows.Count + 1;
+                archive.Sort(archive.Columns[0], ListSortDirection.Descending);
+                if (Instances.DATAPAGE != null)
+                {
+                    Instances.DATAPAGE.LoadComboItems();
+                }
+            }
+
+
+        }
+
+        public void setColumnsWeight()
+        {
+
+            this.archive.ColumnHeadersDefaultCellStyle.BackColor = Color.Black;
+            this.archive.ColumnHeadersDefaultCellStyle.ForeColor = Color.Red;
+            this.archive.EnableHeadersVisualStyles = false;
+            archive.Columns[0].FillWeight = 30;
+            archive.Columns[1].FillWeight = 100;
+            archive.Columns[2].FillWeight = 100;
+            archive.Columns[3].FillWeight = 60;
+            archive.Columns[4].FillWeight = 100;
+            archive.Columns[5].FillWeight = 60;
+            archive.Columns[6].FillWeight = 60;
+            archive.Columns[7].FillWeight = 60;
+            archive.Columns[8].FillWeight = 60;
+            archive.Columns[9].FillWeight = 130;
+            archive.Columns[10].FillWeight = 60;
+            archive.Columns[11].FillWeight = 60;
+            archive.Columns[12].FillWeight = 60;
+        }
+        public void ChangeColor()
+        {
+           
+            foreach (DataGridViewRow row in archive.Rows)
+            {
+                if (row.Cells[7].Value != null)
+                {
+                    if (row.Cells[7].Value.ToString() == "")
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Red;
+                    }
+                }
+                else
+                {
+                    row.DefaultCellStyle.BackColor = Color.Green;
+                }
+
+
+            }
+
+        }
+        private void ArchiveControl_Load(object sender, EventArgs e)
+        {
+            RefreshData();
+            setColumnsWeight();
+
+            
+        }
+
+        private void archive_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            ChangeColor();
+        }
+
+        private void ChercheTXT_Enter(object sender, EventArgs e)
+        {
+
+            if (ChercheTXT.Text == "Chercher...")
+            {
+                ChercheTXT.Text = "";
+            }
+        }
+
+        private void ChercheTXT_Leave(object sender, EventArgs e)
+        {
+            if (ChercheTXT.Text == "")
+            {
+                ChercheTXT.Text = "Chercher...";
+            }
+        }
+
+        private void ModifierBtn_Click(object sender, EventArgs e)
+        {
+            ModifierPopUp mod = new ModifierPopUp(new SelectedModel
+            {
+                matricul = archive.CurrentRow.Cells[0].Value.ToString(),
+                nom = archive.CurrentRow.Cells[1].Value.ToString(),
+                prenom = archive.CurrentRow.Cells[2].Value.ToString(),
+                cin = archive.CurrentRow.Cells[3].Value.ToString(),
+                cnss = archive.CurrentRow.Cells[4].Value.ToString(),
+                rib = archive.CurrentRow.Cells[5].Value.ToString(),
+                dateE = Convert.ToDateTime(archive.CurrentRow.Cells[6].Value.ToString()),
+                tel = archive.CurrentRow.Cells[8].Value.ToString(),
+                site = archive.CurrentRow.Cells[9].Value.ToString(),
+                ville = archive.CurrentRow.Cells[10].Value.ToString(),
+                adresse = archive.CurrentRow.Cells[11].Value.ToString(),
+                obsertion = archive.CurrentRow.Cells[12].Value.ToString()
+
+
+            });
+            mod.Show();
+        }
+        void SearchFor(string name)
+        {
+            Program.access.ClearParam();
+            Program.access.AddParam("@user", "%" + name + "%");
+            var res = Program.access.ExecQuery("SELECT * FROM archive WHERE [N° de Matricul action]  LIKE @user OR Nom LIKE @user OR Prenom  LIKE @user OR [N° de CIN] LIKE @user OR [N° de CNSS] LIKE @user OR [Date d'entré] LIKE @user OR Site LIKE @user OR Ville LIKE @user OR [Date de sortie] LIKE @user OR RIB LIKE @user");
+            if (res.success)
+            {
+                this.archive.DataSource = Program.access.DBDT;
+
+                archive.Sort(archive.Columns[0], ListSortDirection.Descending);
+            }
+            else
+            {
+                RefreshData();
+            }
+            
+        }
+        private void ChercheTXT_TextChanged(object sender, EventArgs e)
+        {
+            if(this.ChercheTXT.Text != "Chercher..." && this.ChercheTXT.Text != string.Empty)
+                SearchFor(this.ChercheTXT.Text);
+        }
+
+        private void SortirBtn_Click(object sender, EventArgs e)
+        {
+            FaireSortir fs = new FaireSortir(archive.CurrentRow.Cells[0].Value.ToString());
+            fs.Show();
+        }
+
+        QueryResult FaireR(string facNum)
+        {
+
+            Program.access.AddParam("@num", facNum);
+
+
+            return Program.access.ExecQuery("UPDATE archive SET [N° de Matricul action] = @num ,[Date de sortie] = Null WHERE [N° de Matricul action] = @num");
+
+        }
+        private void myButton2_Click(object sender, EventArgs e)
+        {
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            // Displays the MessageBox.
+
+            result = MessageBox.Show("Voulez-vous vraiment faire retourner Employé(e) ' "+ archive.CurrentRow.Cells[1].Value.ToString() + " " + archive.CurrentRow.Cells[2].Value.ToString() + " ' ?\nClicker sur 'oui' pour confirmer.", "Confirmation", buttons, MessageBoxIcon.Question);
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                try
+                {
+                    myButton2.Enabled = false;
+                    this.Cursor = Cursors.WaitCursor;
+                    var res = FaireR(archive.CurrentRow.Cells[0].Value.ToString());
+                    if (res.success)
+                    {
+                        Instances.ARCHIVEPAGE.RefreshData();
+
+                        this.Cursor = Cursors.Default;
+                        
+
+
+                    }
+                    else
+                    {
+                        this.Cursor = Cursors.Default;
+                        MessageBox.Show("Erreur survenu : " + res.Exception, "HalfShield : Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    this.Cursor = Cursors.Default;
+                    MessageBox.Show("Erreur survenu : " + ex.Message, "HalfShield : Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+                }
+            }
+        }
+
+        private void archive_SelectionChanged(object sender, EventArgs e)
+        {
+            if (archive.CurrentRow == null)
+                return;
+            if(archive.CurrentRow.Cells[7].Value != null )
+            {
+                if(archive.CurrentRow.Cells[7].Value.ToString() == "")
+                {
+                    myButton2.Enabled = false;
+                }else
+                {
+                    myButton2.Enabled = true ;
+                }
+            }else
+            {
+                myButton2.Enabled = false;
+            }
+        }
+
+        private void myButton1_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            // Displays the MessageBox.
+
+            result = MessageBox.Show("Voulez-vous vraiment supprimer Employé(e) ' " + archive.CurrentRow.Cells[1].Value.ToString() + " " + archive.CurrentRow.Cells[2].Value.ToString() + " ' ?\nClicker sur 'oui' pour confirmer.", "Confirmation", buttons, MessageBoxIcon.Question);
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                try
+                {
+                    myButton1.Enabled = false;
+                    this.Cursor = Cursors.WaitCursor;
+                    Program.access.AddParam("@num", archive.CurrentRow.Cells[0].Value.ToString());
+                    var res = Program.access.ExecQuery("DELETE FROM archive WHERE [N° de Matricul action]=@num ");
+                    if (res.success)
+                    {
+                        Instances.ARCHIVEPAGE.RefreshData();
+                        Properties.Settings.Default.rowsNum--;
+                        Instances.SAISIRPAGE.MatriculTXT.Text =  Program.access.numFacture();
+                        myButton1.Enabled = false;
+                        this.Cursor = Cursors.Default;
+
+
+
+                    }
+                    else
+                    {
+
+                        myButton1.Enabled = false;
+                        this.Cursor = Cursors.Default;
+                        MessageBox.Show("Erreur survenu : " + res.Exception, "HalfShield : Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    myButton1.Enabled = false;
+                    this.Cursor = Cursors.Default;
+                    MessageBox.Show("Erreur survenu : " + ex.Message, "HalfShield : Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+        }
+    }
+}
